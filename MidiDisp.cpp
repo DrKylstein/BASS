6832e642163aae251005e2e903a94f0f321ce7ec
Kyle Delaney <dr.kylstein@gmail.com>
2012-04-03 17:19:31 -0400

    Seems I missed one.

--- MidiDisp.cpp
+++ MidiDisp.cpp
@@ -1,3 +1,25 @@
+/*
+ *  BASS, a MIDI controled synthesizer for MSDOS systems using Adlib or 
+ *  Soundblaster with MPU-401 UART compatible interfaces.
+ *  Copyright (C) 2011  Kyle Delaney
+ *
+ *  This file is a part of BASS.
+ *
+ *  BASS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  BASS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  You may contact the author at <dr.kylstein@gmail.com>
+ */
 #include "MidiDisp.hpp"
 #include "MidiDev.hpp"
 #include "AbstInst.hpp"
